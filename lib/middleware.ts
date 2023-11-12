bd2eb7a47992a67e23dbcba3a2147e849d42cc90
Charles Lowell <cowboyd@frontside.com>
2023-11-12 10:30:41 -0600

    rename serve-assets -> serve-dir, which is what it is

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -2,4 +2,4 @@ export * from "./middleware/assert-html.ts";
 export * from "./middleware/serialize-html.ts";
 export * from "./middleware/island-middleware.ts";
 export * from "./middleware/http-responses.ts";
-export * from "./middleware/serve-assets.ts";
+export * from "./middleware/serve-dir.ts";
--- lib/middleware/serve-dir.ts
+++ lib/middleware/serve-dir.ts
@@ -1,14 +1,13 @@
 import type { HTTPMiddleware } from "../types.ts";
 import { call } from "../deps/effection.ts";
-import { route } from "../route.ts";
 
 import {
   serveDir as server,
   type ServeDirOptions,
 } from "https://deno.land/std@0.206.0/http/file_server.ts";
 
-export function serveAssetsMiddleware(
+export function serveDirMiddleware(
   options?: ServeDirOptions,
 ): HTTPMiddleware {
-  return route("/assets(.*)", (request) => call(server(request, options)));
+  return (request) => call(server(request, options));
 }
