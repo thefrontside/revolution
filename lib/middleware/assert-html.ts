65b9bdcf7be886050536cd8f0e2294a19c397891
Charles Lowell <cowboyd@frontside.com>
2023-11-11 22:29:43 -0600

    import assert-html

--- lib/middleware/assert-html.ts
+++ lib/middleware/assert-html.ts
@@ -1,20 +1,21 @@
-import type { HASTHtmlNode, HASTNode } from "../types.ts";
-import { createMiddleware } from "../handler.ts";
+import type { HASTHtmlNode, JSXElement, Middleware } from "../types.ts";
 
-export const assertHTML = createMiddleware<
+export function assertIsHtmlMiddleware(): Middleware<
   Request,
-  HASTHtmlNode,
-  Request,
-  HASTNode
->(function* (req, next) {
-  let node = yield* next(req);
-  if (node.type !== "element") {
-    throw new Error(
-      `expected an <html> element, but was non-element node of type: '${node.type}'`,
-    );
-  } else if (node.tagName !== "html") {
-    throw new Error(`expected an <html> element, but was <${node.tagName}>`);
-  } else {
-    return node as HASTHtmlNode;
-  }
-});
+HASTHtmlNode,
+Request,
+JSXElement
+> {
+  return function* (req, next) {
+    let node = yield* next(req);
+    if (node.type !== "element") {
+      throw new Error(
+        `expected an <html> element, but was non-element node of type: '${node.type}'`,
+      );
+    } else if (node.tagName !== "html") {
+      throw new Error(`expected an <html> element, but was <${node.tagName}>`);
+    } else {
+      return node as HASTHtmlNode;
+    }
+  };
+}
