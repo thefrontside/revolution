8ec6f170cdfe01840fea4698c0598fe453b10b07
Andy Cunningham <andycunn@gmail.com>
2024-03-28 11:37:17 -0700

    Add http-method-specific routes
    allow middlewares array
    
    Added order of route eval test

--- lib/route.ts
+++ lib/route.ts
@@ -1,4 +1,4 @@
-import type { Middleware } from "./types.ts";
+import type { Handler, Middleware } from "./types.ts";
 
 import {
   match,
@@ -7,24 +7,65 @@ import {
 
 import { createContext, type Operation } from "./deps/effection.ts";
 
-const ParamsContext = createContext<MatchResult["params"]>(
-  "revolution.params",
-);
+const ParamsContext = createContext<MatchResult["params"]>("revolution.params");
 
 export function* useParams<T extends object>(): Operation<T> {
   return (yield* ParamsContext) as T;
 }
 
+function buildMethodRoute<T>(
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH",
+  handler: Handler<Request, T>,
+): Middleware<Request, T> {
+  return function* (request, next) {
+    if (request.method === method) {
+      return yield* handler(request);
+    } else {
+      return yield* next(request);
+    }
+  };
+}
+
+export function GET<T>(handler: Handler<Request, T>): Middleware<Request, T> {
+  return buildMethodRoute("GET", handler);
+}
+
+export function POST<T>(handler: Handler<Request, T>): Middleware<Request, T> {
+  return buildMethodRoute("POST", handler);
+}
+
+export function PUT<T>(handler: Handler<Request, T>): Middleware<Request, T> {
+  return buildMethodRoute("PUT", handler);
+}
+
+export function PATCH<T>(handler: Handler<Request, T>): Middleware<Request, T> {
+  return buildMethodRoute("PATCH", handler);
+}
+
+export function DELETE<T>(
+  handler: Handler<Request, T>,
+): Middleware<Request, T> {
+  return buildMethodRoute("DELETE", handler);
+}
+
 export function route<T>(
   path: string,
-  handler: Middleware<Request, T>,
+  ...middlewares: Middleware<Request, T>[]
 ): Middleware<Request, T> {
+  const inlinedMiddleware = middlewares.slice(1).reduce(
+    (inlined, current) => {
+      return function* (request, next) {
+        return yield* inlined(request, (request) => current(request, next));
+      };
+    },
+    middlewares[0],
+  );
   return function* (request, next) {
     let pathname = new URL(request.url).pathname;
     let result = match(path)(pathname);
     if (result) {
       yield* ParamsContext.set(result.params);
-      return yield* handler(request, next);
+      return yield* inlinedMiddleware(request, next);
     } else {
       return yield* next(request);
     }
